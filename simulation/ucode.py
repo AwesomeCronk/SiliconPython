b891113875edf20a372ba079fdd33f06d7f0112f
Clayton Cronk <awesomecronk@gmail.com>
2022-08-23 12:11:36 -0500

    Added a more convenient way to do state switching in ucode.

--- simulation/ucode.py
+++ simulation/ucode.py
@@ -38,7 +38,25 @@ if __name__ == '__main__':
 
     def encodeStep(step):
         bits = [0] * len(lines)
-        for line in step: bits[len(lines) - lines.index(line) - 1] = 1
+
+        # Easier way of doing a Set_State operation
+        # Requires that State_Set and State_0..State_# be available
+        if len(step) == 1 and step[0][0:11] == 'State_Set>>':
+            # print('Processing quick state change')
+            stateName = step[0][11:]
+            stateNum = states.index(stateName)
+            binStr = bin(stateNum)[2:].zfill(len(bin(numStates)[2:]))
+            # Python uses bit 0 as MSB, Digital uses bit 0 as LSB, have to convert
+            stateLines = ['State_Set']
+            for i in range(len(binStr)):
+                if binStr[-i - 1] == '1': stateLines.append('State_' + str(i))
+            for line in stateLines: bits[len(lines) - lines.index(line) - 1] = 1
+
+        # Normal step encoding
+        else:
+            for line in step: bits[len(lines) - lines.index(line) - 1] = 1
+
+        # Convert bits to bytes and return
         intValue = 0
         for bit in bits: intValue *= 2; intValue += bit
         return int.to_bytes(intValue, len(lines) // 8, 'big')
